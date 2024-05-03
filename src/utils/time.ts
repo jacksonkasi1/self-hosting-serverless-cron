be000f43c629a4e3d240888c15f0d2de502842e2
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-05-03 20:17:52 +0530

    update the time logic

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,11 +1,27 @@
 import cronParser from 'cron-parser';
+import moment from 'moment-timezone';
 
-export function getNextExecutionTime(cronExpression: string): string {
+/**
+ * Adjusts AWS-specific cron expressions to standard cron format by replacing '?' with '*'.
+ * @param {string} awsCronExpression - The AWS cron expression.
+ * @returns {string} - Standard cron expression.
+ */
+function convertAWSCronToStandardCron(awsCronExpression: string): string {
+    return awsCronExpression.replace(/\?/g, '*');
+}
+
+/**
+ * Generates the next execution time in ISO 8601 format from an AWS cron expression.
+ * @param {string} awsCronExpression - The cron expression compatible with AWS EventBridge.
+ * @returns {string} - The next execution time as an ISO 8601 string.
+ */
+export const getNextISO8601FromAWSCron = (awsCronExpression: string): string  => {
     try {
-        const interval = cronParser.parseExpression(cronExpression, { utc: true });
-        return interval.next().toISOString();
+        const standardCronExpression = convertAWSCronToStandardCron(awsCronExpression);
+        const interval = cronParser.parseExpression(standardCronExpression, { utc: true });
+        return moment(interval.next().toDate()).toISOString();
     } catch (error) {
-        console.error('Error parsing cron expression:', error);
-        return new Date().toISOString(); // Fallback to current time if parsing fails
+        console.error('Error generating next execution time:', error);
+        throw new Error('Invalid cron expression');
     }
 }
